977506a6f38ed54ed086d2506fe63b7eac10dfbb
liset29 <79374931315t@gmail.com>
2023-12-15 22:08:06 +0300

    2
    2

--- db.py
+++ db.py
@@ -3,13 +3,22 @@ import sqlite3
 con = sqlite3.connect("teleg_database.db")
 cur = con.cursor()
 lst=[]
+
+def delete_inf(id_chat):
+    cur.execute(f"DELETE FROM users WHERE id_chat={id_chat}")
+    cur.execute(f"DELETE FROM users_mess WHERE id_chat={id_chat}")
+    con.commit()
+
 def date_base(id_chat,surname,message):
     if id_chat not in lst:
+
         name="пукич"
+        message=str(message)
         lst.append(id_chat)
         cur.execute(f"INSERT INTO users (id_chat,surname,name) VALUES({id_chat},\"{surname}\",\"{name}\")")
-    
-    cur.execute(f"INSERT INTO users_mess (id_chat,message) VALUES({id_chat},\"{message}\")")
+        cur.execute(f"INSERT INTO users_mess (id_chat,message) VALUES({id_chat},\"{message}\")")
+    else:    
+        cur.execute(f"UPDATE users_mess SET message=\"{message}\" WHERE users_mess.id_chat={id_chat}")
     
     
     con.commit()
